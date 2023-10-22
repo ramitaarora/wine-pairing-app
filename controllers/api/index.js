31d2673c3ef0c8f89f3c3a35454b6464e93dfbad
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-22 15:46:32 -0700

    deleted homeroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
 const homeRoutes = require('./homeRoutes');
-// const searchRoutes = require('./searchRoutes');
+const searchRoutes = require('./searchRoutes');
 
 router.use('/users', userRoutes);
 router.use('/home', homeRoutes);
-// router.use('/search', searchRoutes);
+router.use('/search', searchRoutes);
 
 module.exports = router;
