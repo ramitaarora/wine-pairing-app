cae964191851609a2ce210add9ae59dcc84b9184
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-22 16:38:10 -0700

    database is seeding

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
-const homeRoutes = require('./homeRoutes');
-const searchRoutes = require('./searchRoutes');
+// const homeRoutes = require('./homeRoutes');
+// const searchRoutes = require('./searchRoutes');
 
 router.use('/users', userRoutes);
-router.use('/home', homeRoutes);
-router.use('/search', searchRoutes);
+// router.use('/home', homeRoutes);
+// router.use('/search', searchRoutes);
 
 module.exports = router;
