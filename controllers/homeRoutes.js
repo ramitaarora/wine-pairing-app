dfea1534f2657358ff20ca25b62a66e941da1939
Christa Lococo <montecitocandleco@gmail.com>
2023-10-23 15:05:34 -0600

    updated attributes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,115 +3,77 @@ const { Food, Wine, Pairing, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-    res.render('landingPage');
+  res.render('landingPage');
 });
 
 router.get('/home', async (req, res) => {
-try{
+  try {
     const pairingData = await Food.findAll({
-        include: [
-            // {
-            // model: User,
-            // attributes: ['name'],
-            // },
-            // {
-            // model: Wine,
-            // attributes: 
-            //             ['name']
-            //             ['type']
-            //             ['price']
-            //             ['description']
-            // },
-            {
-            model: Wine,
-            through: Pairing,
-            // attributes: 
-            //             ['name']
-            //             ['type']
-            //             ['price']
-            //             ['description']
-            },
-        ],
-    }
-    );
+      include: [
+        {
+          model: Wine,
+          through: Pairing,
+        },
+      ],
+    });
 
     const pairings = pairingData.map((pairing) => pairing.get({ plain: true }));
-    // console.log(pairings);
-    res.render('homepage', { pairings, logged_in: req.session.logged_in });
-    //res.json(pairings);//
-    // res.render('homepage', { pairings, logged_in: req.session.logged_in });
 
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    res.render('homepage', { pairings, logged_in: req.session.logged_in });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/pairing/:id', async (req, res) => { //??????
-    try{
-        const pairingData = await Pairing.findByPk(req.params.id, {
-            include:[
-            {
-            model: User,
-            attributes: ['name'],
-            },
-            {
-            model: Wine,
-            attributes: 
-                        ['name']
-                        ['type']
-                        ['price']
-                        ['description']
-            },
-            {
-            model: Food,
-            attributes: 
-                        ['name']
-                        ['type']
-                        ['price']
-                        ['description']
-            },
-            ],
-        });
+router.get('/pairing/:id', async (req, res) => {
+  try {
+    const pairingData = await Pairing.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Wine,
+          attributes: ['name', 'type', 'price', 'description'],
+        },
+        {
+          model: Food,
+          attributes: ['name', 'type', 'price', 'description'],
+        },
+      ],
+    });
 
-        const pairing = pairingData.get({ plain:true });
-       
-        res.json(pairing);
-       
-        // res.render('searchResults', {
-        //     ...pairing,
-        //     logged_in: req.session.logged_in
-        // });
-    } catch (err) {
-        res.status(500).json(err)
-    }
+    const pairing = pairingData.get({ plain: true });
+
+    res.json(pairing);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/profile', withAuth, async (req, res) => {
-    try{
-        const userData = await User.findByPk(req.session.user_id, {
-            attributes: { exclude: ['password'] },
-            include: [{ model: Pairing }],
-        });
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Pairing }],
+    });
 
-        const user = userData.get({ plain: true });
+    const user = userData.get({ plain: true });
 
-        res.json(user);
-        
-        // res.render('profile', {
-        //     ...user,
-        //     logged_in: true
-        // });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/profile');
-        return;
-    }
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+  } else {
     res.render('login');
-})
+  }
+});
 
 module.exports = router;
